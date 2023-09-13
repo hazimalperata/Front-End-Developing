3677694a70cbfe8149ff0fc5569c48840478b372
Hazim Alper Ata <hazimalperata@gmail.com>
2023-09-13 21:05:25 +0300

    COMMIT

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ["./src/**/*.{html,js}"],
+    content: ['./src/**/*.{html,js}',
+        '../**/**/*.{html,js}'],
     theme: {
-        extend: {},
+        extend: {
+            colors: {
+                'blue-gray-900': '#0F172A',
+                'blue-gray-200': '#E2E8F0',
+            },
+            boxShadow: {
+                'light-2xl': '0px 0px 15px 0px rgba(255, 255, 255, 0.07), 0px 25px 50px -12px rgba(255, 255, 255, 0.25)'
+            }
+        },
     },
     plugins: [],
 }
